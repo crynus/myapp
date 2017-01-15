027a08b43c9380f8d7e48d3eb3e6016949928385
crynus <pablo12-92@hotmail.com>
2017-01-15 21:44:05 +0000

    arreglo2

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # Change these
 server '192.168.8.38', port: 22, roles: [:web, :app, :db], primary: true
 
-set :repo_url,        'https://github.com/crynus/myapp'
+set :repo_url,        'https://github.com/crynus/myapp.git'
 set :application,     'myapp'
 set :user,            'root'
 set :puma_threads,    [4, 16]
